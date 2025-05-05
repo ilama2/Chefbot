739740fd98cfcd25c61812e758ad46e9ff84502b
Lama A <109920779+ilama2@users.noreply.github.com>
2025-05-05 22:29:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ from vector_store import add_video_to_vectorstore, create_pinecone_index_if_need
 
 # 🔁 Load environment
 load_dotenv()
-pc = Pinecone(api_key=st.secrets("PINECONE_API_KEY"), environment="us-east-1")
+pc = Pinecone(api_key=st.secrets["PINECONE_API_KEY"], environment="us-east-1")
 index_name = "audios-transcripts"
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7, openai_api_key=st.secrets("OPENAI_API_KEY"))
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7, openai_api_key=st.secrets["OPENAI_API_KEY"])
 
 st.set_page_config(page_title="ChefBot", page_icon="👨‍🍳", layout="wide")
 
